147016f99a6a544f65b26cd641e8f86a79066203
Einar Valur Aðalsteinsson <fizk78@gmail.com>
2016-10-07 10:16:20 +1100

    Update CongressmanController.php

--- src/Althingi/Controller/CongressmanController.php
+++ src/Althingi/Controller/CongressmanController.php
@@ -171,7 +171,7 @@ class CongressmanController extends AbstractRestfulController implements
             ->setStatus(200)
             ->setAllow(['GET', 'OPTIONS'])
             ->setOption('Access-Control-Allow-Origin', '*')
-            ->setOption('Access-Control-Expose-Headers', 'Range, Range-Unit, Content-Range');
+            ->setOption('Access-Control-Allow-Headers', 'Range');
     }
     /**
      * List options for Assembly entry.
@@ -184,7 +184,7 @@ class CongressmanController extends AbstractRestfulController implements
             ->setStatus(200)
             ->setAllow(['GET', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'])
             ->setOption('Access-Control-Allow-Origin', '*')
-            ->setOption('Access-Control-Expose-Headers', 'Range, Range-Unit, Content-Range');
+            ->setOption('Access-Control-Allow-Headers', 'Range');
     }
     
     /**
