cf5b79956e7a88b4cc1bd2d21be1116ea2293900
Einar Valur Aðalsteinsson <fizk78@gmail.com>
2019-11-11 21:33:07 +1100

    Adds ministry (#72)

---
+++ module/Althingi/src/Service/Ministry.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Althingi\Service;
+
+use Althingi\Injector\DatabaseAwareInterface;
+use Althingi\Injector\EventsAwareInterface;
+use Althingi\Model;
+use Althingi\Hydrator;
+use Althingi\Events\AddEvent;
+use Althingi\Events\UpdateEvent;
+use Althingi\Presenters\IndexableAssemblyPresenter;
+use Zend\EventManager\EventManager;
+use Zend\EventManager\EventManagerInterface;
+use PDO;
+
+/**
+ * Class Assembly
+ * @package Althingi\Service
+ */
+class Ministry implements DatabaseAwareInterface, EventsAwareInterface
+{
+    use DatabaseService;
+
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    /** @var \Zend\EventManager\EventManagerInterface */
+    protected $events;
+
+    /**
+     * Get one Assembly.
+     *
+     * @param $id
+     * @return null|\Althingi\Model\Ministry
+     */
+    public function get(int $id): ? Model\Ministry
+    {
+        $statement = $this->getDriver()->prepare("select * from `Ministry` where ministry_id = :id");
+        $statement->execute(['id' => $id]);
+        $assembly = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $assembly
+            ? (new Hydrator\Ministry)->hydrate($assembly, new Model\Ministry())
+            : null;
+    }
+
+    /**
+     * Get all Assemblies.
+     *
+     * @return \Althingi\Model\Ministry[]
+     */
+    public function fetchAll(): array
+    {
+
+        $statement = $this->getDriver()->prepare("select * from `Ministry`");
+        $statement->execute();
+
+        return array_map(function ($assembly) {
+            return (new Hydrator\Ministry)->hydrate($assembly, new Model\Ministry());
+        }, $statement->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+    /**
+     * Count all assemblies.
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        $statement = $this->getDriver()->prepare("select count(*) from `Ministry`");
+        $statement->execute();
+
+        return (int) $statement->fetchColumn(0);
+    }
+
+    /**
+     * Create one entry.
+     *
+     * @param \Althingi\Model\Ministry $data
+     * @return int affected rows
+     */
+    public function create(Model\Ministry $data): int
+    {
+        $statement = $this->getDriver()->prepare(
+            $this->toInsertString('Ministry', $data)
+        );
+        $statement->execute($this->toSqlValues($data));
+
+//        $this->getEventManager()
+//            ->trigger(
+//                AddEvent::class,
+//                new AddEvent(new IndexableAssemblyPresenter($data)),
+//                ['rows' => $statement->rowCount()]
+//            );
+
+        return $this->getDriver()->lastInsertId();
+    }
+
+    /**
+     * Save one entry.
+     *
+     * @param \Althingi\Model\Ministry $data
+     * @return int affected rows
+     */
+    public function save(Model\Ministry $data): int
+    {
+        $statement = $this->getDriver()->prepare(
+            $this->toSaveString('Ministry', $data)
+        );
+        $statement->execute($this->toSqlValues($data));
+
+//        switch ($statement->rowCount()) {
+//            case 1:
+//                $this->getEventManager()
+//                    ->trigger(
+//                        AddEvent::class,
+//                        new AddEvent(new IndexableAssemblyPresenter($data)),
+//                        ['rows' => $statement->rowCount()]
+//                    );
+//                break;
+//            case 0:
+//            case 2:
+//                $this->getEventManager()
+//                    ->trigger(
+//                        UpdateEvent::class,
+//                        new UpdateEvent(new IndexableAssemblyPresenter($data)),
+//                        ['rows' => $statement->rowCount()]
+//                    );
+//                break;
+//        }
+        return $statement->rowCount();
+    }
+
+    /**
+     * Update one entry.
+     *
+     * @param \Althingi\Model\Ministry|object $data
+     * @return int affected rows
+     */
+    public function update(Model\Ministry $data): int
+    {
+        $statement = $this->getDriver()->prepare(
+            $this->toUpdateString('Ministry', $data, "ministry_id={$data->getMinistryId()}")
+        );
+        $statement->execute($this->toSqlValues($data));
+
+//        $this->getEventManager()
+//            ->trigger(
+//                UpdateEvent::class,
+//                new UpdateEvent(new IndexableAssemblyPresenter($data)),
+//                ['rows' => $statement->rowCount()]
+//            );
+
+        return $statement->rowCount();
+    }
+
+    /**
+     * Delete one ministry_id.
+     * Should return 1, for one assembly deleted.
+     *
+     * @param int $id
+     * @return int
+     */
+    public function delete(int $id): int
+    {
+        $statement = $this->getDriver()->prepare("delete from `Ministry` where ministry_id = :ministry_id");
+        $statement->execute(['ministry_id' => $id]);
+
+        return $statement->rowCount();
+    }
+
+    /**
+     * @param \PDO $pdo
+     * @return $this
+     */
+    public function setDriver(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+        return $this;
+    }
+
+    /**
+     * @return \PDO
+     */
+    public function getDriver()
+    {
+        return $this->pdo;
+    }
+
+    public function setEventManager(EventManagerInterface $events)
+    {
+        $this->events = $events;
+        return $this;
+    }
+
+    public function getEventManager()
+    {
+        if (null === $this->events) {
+            $this->setEventManager(new EventManager());
+        }
+        return $this->events;
+    }
+}
---
+++ module/Althingi/tests/Service/MinistryTest.php
@@ -0,0 +1,280 @@
+<?php
+
+namespace AlthingiTest\Service;
+
+use Althingi\Service\Ministry;
+use AlthingiTest\DatabaseConnection;
+use Althingi\Model;
+use PHPUnit\Framework\TestCase;
+
+class MinistryTest extends TestCase
+{
+    use DatabaseConnection;
+
+    /** @var  \PDO */
+    private $pdo;
+
+    public function testGet()
+    {
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+
+        $expectedData = (new Model\Ministry())
+            ->setMinistryId(1)
+            ->setName('name 1')
+            ->setAbbrShort('abbr_short1')
+            ->setAbbrLong('abbr_long1')
+            ->setFirst(1)
+            ->setLast(3);
+
+        $actualData = $ministryService->get(1);
+
+        $this->assertEquals($expectedData, $actualData);
+    }
+
+    public function testGetNotFound()
+    {
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+
+        $actualData = $ministryService->get(100);
+
+        $this->assertNull($actualData);
+    }
+
+    public function testFetch()
+    {
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+
+        $expectedData = [
+            (new Model\Ministry())
+                ->setMinistryId(1)
+                ->setName('name 1')
+                ->setAbbrShort('abbr_short1')
+                ->setAbbrLong('abbr_long1')
+                ->setFirst(1)
+                ->setLast(3),
+            (new Model\Ministry())
+                ->setMinistryId(2)
+                ->setName('name 2')
+                ->setAbbrShort('abbr_short2')
+                ->setAbbrLong('abbr_long2')
+                ->setFirst(1)
+                ->setLast(null)
+        ];
+        $actualData = $ministryService->fetchAll();
+
+        $this->assertEquals($expectedData, $actualData);
+    }
+
+    public function testCreate()
+    {
+        $ministry = (new Model\Ministry())
+            ->setMinistryId(3)
+            ->setName('name 3')
+            ->setAbbrShort('abbr_short3')
+            ->setAbbrLong('abbr_long3');
+
+        $expectedTable = $this->createArrayDataSet([
+            'Ministry' => [
+                [
+                    'ministry_id' => 1,
+                    'name' => 'name 1',
+                    'abbr_short' => 'abbr_short1',
+                    'abbr_long' => 'abbr_long1',
+                    'first' => 1,
+                    'last' => 3,
+                ],
+                [
+                    'ministry_id' => 2,
+                    'name' => 'name 2',
+                    'abbr_short' => 'abbr_short2',
+                    'abbr_long' => 'abbr_long2',
+                    'first' => 1,
+                    'last' => null,
+                ],
+                [
+                    'ministry_id' => 3,
+                    'name' => 'name 3',
+                    'abbr_short' => 'abbr_short3',
+                    'abbr_long' => 'abbr_long3',
+                    'first' => null,
+                    'last' => null,
+                ],
+            ],
+        ])->getTable('Ministry');
+        $actualTable = $this->getConnection()->createQueryTable('Ministry', 'SELECT * FROM Ministry');
+
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+        $ministryService->create($ministry);
+
+        $this->assertTablesEqual($expectedTable, $actualTable);
+    }
+
+    public function testSaveUpdate()
+    {
+        $ministry = (new Model\Ministry())
+            ->setMinistryId(2)
+            ->setName('new name')
+            ->setAbbrShort('new abbr short')
+            ->setAbbrLong('new abbr long')
+            ->setFirst(1)
+        ;
+
+        $expectedTable = $this->createArrayDataSet([
+            'Ministry' => [
+                [
+                    'ministry_id' => 1,
+                    'name' => 'name 1',
+                    'abbr_short' => 'abbr_short1',
+                    'abbr_long' => 'abbr_long1',
+                    'first' => 1,
+                    'last' => 3,
+                ],
+                [
+                    'ministry_id' => 2,
+                    'name' => 'new name',
+                    'abbr_short' => 'new abbr short',
+                    'abbr_long' => 'new abbr long',
+                    'first' => 1,
+                    'last' => null,
+                ],
+            ],
+        ])->getTable('Ministry');
+        $actualTable = $this->getConnection()->createQueryTable('Ministry', 'SELECT * FROM Ministry');
+
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+        $affectedRows = $ministryService->save($ministry);
+
+        $this->assertTablesEqual($expectedTable, $actualTable);
+        $this->assertEquals(2, $affectedRows);
+    }
+
+    public function testSaveCreate()
+    {
+        $ministry = (new Model\Ministry())
+            ->setMinistryId(3)
+            ->setName('new name')
+            ->setAbbrShort('new abbr short')
+            ->setAbbrLong('new abbr long')
+            ->setFirst(1)
+        ;
+
+        $expectedTable = $this->createArrayDataSet([
+            'Ministry' => [
+                [
+                    'ministry_id' => 1,
+                    'name' => 'name 1',
+                    'abbr_short' => 'abbr_short1',
+                    'abbr_long' => 'abbr_long1',
+                    'first' => 1,
+                    'last' => 3,
+                ],
+                [
+                    'ministry_id' => 2,
+                    'name' => 'name 2',
+                    'abbr_short' => 'abbr_short2',
+                    'abbr_long' => 'abbr_long2',
+                    'first' => 1,
+                    'last' => null,
+                ],
+                [
+                    'ministry_id' => 3,
+                    'name' => 'new name',
+                    'abbr_short' => 'new abbr short',
+                    'abbr_long' => 'new abbr long',
+                    'first' => 1,
+                    'last' => null,
+                ],
+            ],
+        ])->getTable('Ministry');
+        $actualTable = $this->getConnection()->createQueryTable('Ministry', 'SELECT * FROM Ministry');
+
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+        $affectedRows = $ministryService->save($ministry);
+
+        $this->assertTablesEqual($expectedTable, $actualTable);
+        $this->assertEquals(1, $affectedRows);
+    }
+
+    public function testUpdate()
+    {
+        $ministry = (new Model\Ministry())
+            ->setMinistryId(2)
+            ->setName('new name')
+            ->setAbbrShort('new abbr short')
+            ->setAbbrLong('new abbr long')
+            ->setFirst(1)
+        ;
+
+        $expectedTable = $this->createArrayDataSet([
+            'Ministry' => [
+                [
+                    'ministry_id' => 1,
+                    'name' => 'name 1',
+                    'abbr_short' => 'abbr_short1',
+                    'abbr_long' => 'abbr_long1',
+                    'first' => 1,
+                    'last' => 3,
+                ],
+                [
+                    'ministry_id' => 2,
+                    'name' => 'new name',
+                    'abbr_short' => 'new abbr short',
+                    'abbr_long' => 'new abbr long',
+                    'first' => 1,
+                    'last' => null,
+                ],
+            ],
+        ])->getTable('Ministry');
+        $actualTable = $this->getConnection()->createQueryTable('Ministry', 'SELECT * FROM Ministry');
+
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+        $ministryService->update($ministry);
+
+
+        $this->assertTablesEqual($expectedTable, $actualTable);
+    }
+
+    public function testCount()
+    {
+        $ministryService = new Ministry();
+        $ministryService->setDriver($this->pdo);
+
+        $this->assertEquals(2, $ministryService->count());
+    }
+
+    protected function getDataSet()
+    {
+        return $this->createArrayDataSet([
+            'Assembly' => [
+                ['assembly_id' => 1, 'from' => '2000-01-01', 'to' => null],
+                ['assembly_id' => 2, 'from' => '2000-01-01', 'to' => null],
+                ['assembly_id' => 3, 'from' => '2000-01-01', 'to' => null],
+            ],
+            'Ministry' => [
+                [
+                    'ministry_id' => 1,
+                    'name' => 'name 1',
+                    'abbr_short' => 'abbr_short1',
+                    'abbr_long' => 'abbr_long1',
+                    'first' => 1,
+                    'last' => 3,
+                ],
+                [
+                    'ministry_id' => 2,
+                    'name' => 'name 2',
+                    'abbr_short' => 'abbr_short2',
+                    'abbr_long' => 'abbr_long2',
+                    'first' => 1,
+                    'last' => null,
+                ],
+            ],
+        ]);
+    }
+}
