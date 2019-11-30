3d69b8386a11c0b5e4621147436ed6d18942c2d1
Einar Adalsteinsson <fizk78@gmail.com>
2019-12-01 10:21:04 +1100

    Adds an indexer for minister sittings

--- auto/index-assembly.sh
+++ auto/index-assembly.sh
@@ -3,4 +3,5 @@
 SCRIPT=$( cd "$( dirname "$0" )" && pwd )/../public/index.php
 
 php ${SCRIPT} index:session --assembly=$1
+php ${SCRIPT} index:ministry-sitting --assembly=$1
 php ${SCRIPT} index:assembly --assembly=$1
--- module/Althingi/src/Controller/Console/IndexerIssueController.php
+++ module/Althingi/src/Controller/Console/IndexerIssueController.php
@@ -8,16 +8,19 @@ use Althingi\Injector\ServiceAssemblyAwareInterface;
 use Althingi\Injector\ServiceDocumentAwareInterface;
 use Althingi\Injector\ServiceIssueAwareInterface;
 use Althingi\Injector\ServiceIssueCategoryAwareInterface;
+use Althingi\Injector\ServiceMinisterSittingAwareInterface;
 use Althingi\Injector\ServiceProponentAwareInterface;
 use Althingi\Injector\ServiceSessionAwareInterface;
 use Althingi\Injector\ServiceSpeechAwareInterface;
 use Althingi\Injector\ServiceVoteAwareInterface;
 use Althingi\Injector\ServiceVoteItemAwareInterface;
+use Althingi\Injector\StallingAwareInterface;
 use Althingi\Presenters\IndexableAssemblyPresenter;
 use Althingi\Presenters\IndexableCongressmanDocumentPresenter;
 use Althingi\Presenters\IndexableDocumentPresenter;
 use Althingi\Presenters\IndexableIssueCategoryPresenter;
 use Althingi\Presenters\IndexableIssuePresenter;
+use Althingi\Presenters\IndexableMinisterSittingPresenter;
 use Althingi\Presenters\IndexableSessionPresenter;
 use Althingi\Presenters\IndexableSpeechPresenter;
 use Althingi\Presenters\IndexableVoteItemPresenter;
@@ -27,6 +30,7 @@ use Althingi\Service\CongressmanDocument;
 use Althingi\Service\Document;
 use Althingi\Service\Issue;
 use Althingi\Service\IssueCategory;
+use Althingi\Service\MinisterSitting;
 use Althingi\Service\Session;
 use Althingi\Service\Speech;
 use Althingi\Service\Vote;
@@ -36,8 +40,6 @@ use Althingi\Events\AddEvent;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use Psr\Log\LoggerInterface;
 use Zend\Mvc\Controller\AbstractActionController;
-use Zend\ProgressBar\ProgressBar;
-use Zend\ProgressBar\Adapter\Console;
 
 class IndexerIssueController extends AbstractActionController implements
     ServiceSpeechAwareInterface,
@@ -49,8 +51,10 @@ class IndexerIssueController extends AbstractActionController implements
     ServiceAssemblyAwareInterface,
     ServiceVoteItemAwareInterface,
     ServiceSessionAwareInterface,
+    ServiceMinisterSittingAwareInterface,
     LoggerAwareInterface,
-    QueueAwareInterface
+    QueueAwareInterface,
+    StallingAwareInterface
 {
     /** @var  \Althingi\Service\Speech */
     private $speechService;
@@ -79,15 +83,17 @@ class IndexerIssueController extends AbstractActionController implements
     /** @var  \Althingi\Service\Session */
     private $sessionService;
 
+    /** @var  \Althingi\Service\MinisterSitting */
+    private $ministerSittingService;
+
     /** @var \Psr\Log\LoggerInterface */
     private $logger;
 
+    /** @var int */
+    private $sleeptime = 150000;
+
     /** @var \PhpAmqpLib\Connection\AMQPStreamConnection  */
     protected $client;
-
-    private $sleepTime = 1000;
-    private $processedCount = 0;
-    private $processedSize = 1000;
 
     /**
      *
@@ -109,7 +115,7 @@ class IndexerIssueController extends AbstractActionController implements
 
         $addEvent = new Add($this->getQueue(), $this->getLogger());
         $addEvent(new AddEvent(new IndexableAssemblyPresenter($assembly), ['rows' => 1]));
-        usleep(150000);
+        usleep($this->sleeptime);
 
         $issues = $this->issueService->fetchAllByAssembly($assembly->getAssemblyId());
         foreach ($issues as $issue) {
@@ -126,7 +132,20 @@ class IndexerIssueController extends AbstractActionController implements
         $addEvent = new Add($this->getQueue(), $this->getLogger());
         foreach ($sessions as $session) {
             $addEvent(new AddEvent(new IndexableSessionPresenter($session), ['rows' => 1]));
-            usleep(150000);
+            usleep($this->sleeptime);
+        }
+    }
+
+    public function ministrySittingAction()
+    {
+        $assemblyId = $this->params('assembly');
+
+        $sessions = $this->ministerSittingService->fetchByAssembly($assemblyId);
+
+        $addEvent = new Add($this->getQueue(), $this->getLogger());
+        foreach ($sessions as $session) {
+            $addEvent(new AddEvent(new IndexableMinisterSittingPresenter($session), ['rows' => 1]));
+            usleep($this->sleeptime);
         }
     }
 
@@ -134,37 +153,37 @@ class IndexerIssueController extends AbstractActionController implements
     {
         $addEvent = new Add($this->getQueue(), $this->getLogger());
         $addEvent(new AddEvent(new IndexableIssuePresenter($issue), ['rows' => 1]));
-        usleep(150000);
+        usleep($this->sleeptime);
 
         $categories = $this->issueCategoryService->fetchByIssue($issue->getAssemblyId(), $issue->getIssueId());
         foreach ($categories as $category) {
             $addEvent(new AddEvent(new IndexableIssueCategoryPresenter($category), ['rows' => 1]));
-            usleep(150000);
+            usleep($this->sleeptime);
         }
 
         if ($issue->isA()) {
             $documents = $this->documentService->fetchByIssue($issue->getAssemblyId(), $issue->getIssueId());
             foreach ($documents as $document) {
                 $addEvent(new AddEvent(new IndexableDocumentPresenter($document), ['rows' => 1]));
-                usleep(150000);
+                usleep($this->sleeptime);
 
                 $proponents = $this->congressmanDocumentService
                     ->fetchByDocument($issue->getAssemblyId(), $issue->getIssueId(), $document->getDocumentId());
                 foreach ($proponents as $proponent) {
                     $addEvent(new AddEvent(new IndexableCongressmanDocumentPresenter($proponent), ['rows' => 1]));
-                    usleep(150000);
+                    usleep($this->sleeptime);
                 }
 
                 $votes = $this->voteService
                     ->fetchByDocument($issue->getAssemblyId(), $issue->getIssueId(), $document->getDocumentId());
                 foreach ($votes as $vote) {
                     $addEvent(new AddEvent(new IndexableVotePresenter($vote), ['rows' => 1]));
-                    usleep(150000);
+                    usleep($this->sleeptime);
 
                     $voteItems = $this->voteItemService->fetchByVote($vote->getVoteId());
                     foreach ($voteItems as $voteItem) {
                         $addEvent(new AddEvent(new IndexableVoteItemPresenter($voteItem), ['rows' => 1]));
-                        usleep(150000);
+                        usleep($this->sleeptime);
                     }
                 }
             }
@@ -174,7 +193,7 @@ class IndexerIssueController extends AbstractActionController implements
             ->fetchAllByIssue($issue->getAssemblyId(), $issue->getIssueId(), $issue->getCategory());
         foreach ($speeches as $speech) {
             $addEvent(new AddEvent(new IndexableSpeechPresenter($speech), ['rows' => 1]));
-            usleep(150000);
+            usleep($this->sleeptime);
         }
     }
 
@@ -288,14 +307,6 @@ class IndexerIssueController extends AbstractActionController implements
         return $this;
     }
 
-    private function shouldSleep()
-    {
-        $this->processedCount++;
-        if ($this->processedCount % $this->processedSize === 0) {
-            usleep($this->sleepTime);
-        }
-    }
-
     /**
      * @param Session $session
      * @return $this
@@ -303,6 +314,26 @@ class IndexerIssueController extends AbstractActionController implements
     public function setSessionService(Session $session)
     {
         $this->sessionService = $session;
+        return $this;
+    }
+
+    /**
+     * @param \Althingi\Service\MinisterSitting $ministerSitting
+     * @return $this
+     */
+    public function setMinisterSittingService(MinisterSitting $ministerSitting)
+    {
+        $this->ministerSittingService = $ministerSitting;
+        return $this;
+    }
+
+    /**
+     * @param int $time
+     * @return $this
+     */
+    public function setStallTime(int $time)
+    {
+        $this->sleeptime = $time;
         return $this;
     }
 }
