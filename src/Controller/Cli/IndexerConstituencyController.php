4c19a4f1158f7453c03405697cd17b135e2f9c72
Einar Adalsteinsson <fizk78@gmail.com>
2021-06-04 10:37:15 +1000

    Adds indexing for Party and Constituency

---
+++ src/Controller/Cli/IndexerConstituencyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Althingi\Controller\Cli;
+
+use Althingi\Events\AddEvent;
+use Althingi\Utils\ConsoleResponse;
+use Althingi\Injector\{
+    EventsAwareInterface,
+    ServiceConstituencyAwareInterface,
+};
+use Althingi\Presenters\IndexableConstituencyPresenter;
+use Althingi\Service\Constituency;
+use Psr\Http\Message\{
+    ServerRequestInterface,
+    ResponseInterface
+};
+
+use Althingi\Service\EventService;
+
+class IndexerConstituencyController implements ServiceConstituencyAwareInterface, EventsAwareInterface
+{
+    use EventService;
+    private Constituency $constituencyService;
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        foreach ($this->constituencyService->fetchAllGenerator() as $model) {
+            $this->getEventDispatcher()->dispatch(
+                new AddEvent(new IndexableConstituencyPresenter($model), ['rows' => 1]),
+            );
+        }
+
+        return (new ConsoleResponse(__CLASS__));
+    }
+
+    public function setConstituencyService(Constituency $constituency): self
+    {
+        $this->constituencyService = $constituency;
+        return $this;
+    }
+}
---
+++ src/Controller/Cli/IndexerPartyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Althingi\Controller\Cli;
+
+use Althingi\Events\AddEvent;
+use Althingi\Utils\ConsoleResponse;
+use Althingi\Injector\{
+    EventsAwareInterface,
+    ServicePartyAwareInterface
+};
+use Althingi\Presenters\IndexablePartyPresenter;
+use Psr\Http\Message\{
+    ServerRequestInterface,
+    ResponseInterface
+};
+
+use Althingi\Service\EventService;
+use Althingi\Service\Party;
+
+class IndexerPartyController implements ServicePartyAwareInterface, EventsAwareInterface
+{
+    use EventService;
+    private Party $partyService;
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        foreach ($this->partyService->fetchAllGenerator() as $model) {
+            $this->getEventDispatcher()->dispatch(
+                new AddEvent(new IndexablePartyPresenter($model), ['rows' => 1]),
+            );
+        }
+
+        return (new ConsoleResponse(__CLASS__));
+    }
+
+    public function setPartyService(Party $party): self
+    {
+        $this->partyService = $party;
+        return $this;
+    }
+}
