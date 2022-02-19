68ab8c374535e8e6a0b0bf48c10acbd8916e5204
Einar Adalsteinsson <fizk78@gmail.com>
2022-02-19 17:07:35 +1100

    Set SQL mode NO_AUTO_VALUE_ON_ZERO

--- config/service.php
+++ config/service.php
@@ -434,7 +434,7 @@ return [
                 [
                     PDO::MYSQL_ATTR_INIT_COMMAND =>
                     "SET NAMES 'utf8', " .
-                    "sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'",
+                    "sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION,NO_AUTO_VALUE_ON_ZERO';",
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                 ]
